b72cc014d5f14cb35396ff09f99aa676469715eb
Aashiq <adams.aashiq@gmail.com>
2021-04-26 15:58:57 +0200

    started login

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,10 @@
 <template>
-    <div class="q-pa-md" style="max-width: 400px">
+  <div class="q-pa-md">
+    <div class="q-gutter-md" style="max-width: 300px">
+      <q-input standout="bg-teal text-white" v-model="text" label="Username/Email" />
 
-    <q-form
-      @submit="onSubmit"
-      @reset="onReset"
-      class="q-gutter-md"
-    >
-      <q-input
-        filled
-        v-model="name"
-        label="Your name *"
-        hint="Name and surname"
-        lazy-rules
-        :rules="[ val => val && val.length > 0 || 'Please type something']"
-      />
-
-      <q-input
-        filled
-        type="number"
-        v-model="age"
-        label="Your age *"
-        lazy-rules
-        :rules="[
-          val => val !== null && val !== '' || 'Please type your age',
-          val => val > 0 && val < 100 || 'Please type a real age'
-        ]"
-      />
-
-      <q-toggle v-model="accept" label="I accept the license and terms" />
-
-      <div>
-        <q-btn label="Submit" type="submit" color="primary"/>
-        <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
-      </div>
-    </q-form>
-
+      <q-input standout="bg-teal text-white" v-model="text" label="Password" />
+    </div>
   </div>
 </template>
 
@@ -43,13 +13,19 @@ import { ref } from 'vue'
 
 export default {
   name: 'LayoutDefault',
-
+  data () {
+      return {
+        text: ''
+      }
+    },
+  methods: {
+     
+  },
   components: {
   },
 
   setup () {
     return {
-      leftDrawerOpen: ref(false)
     }
   }
 }
