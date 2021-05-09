8fc9a2345c76c46fa8f8de095333380fbe6749e4
Chad <61545754+ChadH28@users.noreply.github.com>
2021-05-10 00:14:23 +0200

    ran audit fixes installed aos and worked on user profile

--- src/components/CreateBlog.vue
+++ src/components/CreateBlog.vue
@@ -18,7 +18,7 @@
       <div>
         <q-btn label="Post" type="submit" color="primary" />
         <q-btn
-          label="Reset"
+          :icon="'fas fa-redo-alt'"
           type="reset"
           color="primary"
           flat
